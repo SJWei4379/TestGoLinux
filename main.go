42bc03ddb9cd4c28f7830ea155d9b2dbb25ca18a
SJWei4379 <127107484+SJWei4379@users.noreply.github.com>
2023-04-08 10:55:36 +0800

    Add files via upload

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+
+	fmt.Fprintf(w, "hello, world\n")
+	//io.WriteString(w, "Hello, world!\n")
+	fmt.Println("成功通过ip访问！！！")
+}
+
+
+func main() {
+
+	fmt.Println("成功部署到服务器！")
+	http.HandleFunc("/hello", hello)
+
+	err := http.ListenAndServe(":8888", nil)
+
+	if err != nil {
+
+		panic(err)
+
+	}
+
+}
