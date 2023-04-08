5ce3f8e4fd77bc1e9ea10f12aa70cb45758c98b9
Weishengji <2026473319@qq.com>
2023-04-08 19:39:34 +0800

    测试

--- main.go
+++ main.go
@@ -8,10 +8,10 @@ import (
 func hello(w http.ResponseWriter, req *http.Request) {
 
 	fmt.Fprintf(w, "hello, world\n")
+	fmt.Fprintf(w, "恭喜！项目运行成功！！！")
 	//io.WriteString(w, "Hello, world!\n")
 	fmt.Println("成功通过ip访问！！！")
 }
-
 
 func main() {
 
